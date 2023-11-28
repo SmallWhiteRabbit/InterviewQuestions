76a818586129109eb4c13c30f47dac35158a81cf
zjr <1377455672@qq.com>
2023-11-28 17:36:26 +0800

    初始刷

---
+++ src/views/createPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <a> main page</a>
+  <div></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/views/mainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>current status:{{}}</div>
+  <div>update Status：{{}}</div>
+  <div>
+    Could be transferred to
+    <select></select>
+  </div>
+  <div>
+    How could be transferred to
+    <select></select>
+  </div>
+  <div>Transferred to</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
